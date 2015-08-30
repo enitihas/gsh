66fac9d0277e6858b13256a30f38ba05592d2f32
Abhishek Pandey <enitihas@gmail.com>
2015-08-30 18:04:23 +0530

    Changed promopt to show tilde for home and use LOGNAME for use

--- src/Shell.py
+++ src/Shell.py
@@ -15,7 +15,7 @@ class Shell:
 
     def __init__(self):
         self.path = os.environ['PATH'].split(':')
-        self.user = os.environ['HOME'].split('/')[-1]#os.getlogin()
+        self.user = os.environ['LOGNAME']
         self.cwd = os.getcwd()
         self.prompt_function = input
         self.builtins = {
@@ -37,7 +37,7 @@ class Shell:
 
     @property
     def prompt(self):
-        return self.user + '->' + self.cwd + ':$'
+        return self.user + '->' + re.sub(os.environ['HOME'],'~',self.cwd) + ':$'
         # + os.getcwd().replace('/home/'+self.user, '~') + '$:'
 
     @staticmethod
@@ -56,7 +56,6 @@ class Shell:
         except FileNotFoundError:
             print('cd: no such file or directory:', dest)
             return
-        sub = re.sub(r'[~]',os.getenv('HOME'),dest)
         if dest[0] == '/':
             self.cwd = dest
         else:
